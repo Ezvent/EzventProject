5bb3280fc16a82b8693b6ac9a9aae4eb318bf462
GavansSEGet <gphitides1@student.gsu.edu>
2021-11-13 00:17:41 +0000

    Set up routes and a basic frontend scaffolding

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ from flask_login import (
 app = flask.Flask(__name__, static_folder="./build/static")
 bp = flask.Blueprint("bp", __name__, template_folder="./build")
 
+###delete this### 
+isAdmin = True
+
 
 @bp.route("/index")
 # @login_required
@@ -33,5 +36,126 @@ def home():
 
 
 app.register_blueprint(bp)
+
+
+#region Login Routes
+
+
+@app.route("/")
+def index():
+    return flask.render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    
+    user = flask.request.get_json()["user"]
+    pswd = flask.request.get_json()["pswd"]
+
+    #password matches?
+    print(user)
+    print(pswd)
+
+    return flask.redirect("/login/updatecalendar")
+
+@app.route("/login/updatecalendar")
+def update_google_calendar():
+    # update calendar logic
+    # check if user is admin
+    if(isAdmin):
+        return flask.redirect("/adminhomepage")
+    return flask.redirect("/userhomepage")
+
+#endregion
+ 
+
+
+
+
+
+
+
+#region User Homepage routes
+
+# we need to figure out at when to get the pending requests to load it up when the page opens
+@app.route("/userhomepage")
+def render_userhomepage():
+    # probably going to try and get the pending events here with a call to "somefile.py"
+    return flask.render_template("userhomepage.html")
+    
+
+@app.route("/acceptevent")
+def accept_event_request():
+    # add event with user's nickname or email to calendar
+    # remove from events pending list
+
+    return 0 # not sure how to handle the return here
+
+
+@app.route("/rejectevent")
+def reject_event_request():
+    # remove from events pending list
+    # notify admin of rejection
+
+    return 0 # again not sure how to handle the return here
+
+#endregion
+
+
+
+
+
+
+
+
+#region Admin Homepage routes
+
+@app.route("/adminhomepage")
+def render_adminhomepage():
+    return flask.render_template("adminhomepage.html")
+
+@app.route("/createuser")
+def create_user():
+    # change the swappable window element to "create new user" template
+    # add new user to user database logic
+    return flask.render_template("adminhomepage.html")
+
+@app.route("/removeuser")
+def render_adminhomepage():
+    #  change the swappable window element to "list of users you can remove" template
+    #  remove user from database logic
+   return flask.render_template("adminhomepage.html")
+
+@app.route("/showcalendar")
+def render_calendar():
+    #change swappable window element to show the google calendar
+    #update the google calendar state
+    return 0 # also not sure what to return here
+
+@app.route("/createeventrequest")
+def create_new_event():
+    #change swappable window element to show the the "create new event" template
+    #create new event Logic
+    return flask.redirect("/sendoutnewrequest")
+  
+@app.route("/sendoutnewrequest")
+def send_request_to_users():
+    #send new request to users logic
+    return flask.render_template("adminhomepage.html")
+
+#endregion
+
+#region Routes for both employees and Admin
+
+@app.route("/changepassword")
+def change_password():
+    #change password logic
+    return flask.redirect("/userhomepage")
+
+@app.route("/logout")
+def logout_session():
+    #logout logic
+    return flask.redirect("/")
+
+#endregion
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8081)), debug=True)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const setUsernameForm = (e) => {
+    setUsername(e.target.value.trim());
+  }
+
+  const setPasswordForm = (e) => {
+    setPassword(e.target.value.trim());
+  }
+
+  const submitLogin = () => {
+    console.log(username);
+    console.log(password);
+
+    const data = {
+      'username': username,
+      'password': password
+    }
+
+    console.log(data);
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+                              'user': username,
+                              'pswd': password
+                            })
+    }).then(response => response.json().then(newData => {
+        console.log(newData);
+    }))
+  }
+
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+          <h1 id="login-header">Login</h1>
+          
+          <div id="login-error-msg-holder">
+            <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+          </div>
+          
+          <form id="login-form">
+            <input type="text" name="username" id="username-field" onChange={ setUsernameForm } class="login-form-field" placeholder="Username"/>
+            <input type="password" name="password" id="password-field" onChange={ setPasswordForm } class="login-form-field" placeholder="Password"/>
+          </form>
+          <button onClick={ submitLogin }>Submit</button>
     </div>
   );
 }
