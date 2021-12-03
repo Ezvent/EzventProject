860d02536639e2219174419366afcbfa1d25ce9c
Shaheen <shaheensr@outlook.com>
2021-12-03 16:39:37 -0500

    Eslint formatting done.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,20 @@
+/* eslint linebreak-style: ["error", "windows"] */
 import React from 'react';
 
-const Calendar = ({ email }) => {
-
-    return (
-        <div>
-            <p></p>
-            <p></p>
-            <iframe
-                src={"https://calendar.google.com/calendar/embed?src=" + encodeURI("qb4oq3kl7h98fvkvudmq98mpqg@group.calendar.google.com")}
-                style={{ border: "solid 1px #777", width: "500px", height: "300px", frameborder: "0", scrolling: "no" }}>
-            </iframe>
-        </div>
-    )
+const Calendar = function () {
+  return (
+    <div>
+      <p />
+      <p />
+      <iframe
+        title="Calendar"
+        src={`https://calendar.google.com/calendar/embed?src=${encodeURI('qb4oq3kl7h98fvkvudmq98mpqg@group.calendar.google.com')}`}
+        style={{
+          border: 'solid 1px #777', width: '500px', height: '300px', frameborder: '0', scrolling: 'no',
+        }}
+      />
+    </div>
+  );
 };
 
 export default Calendar;
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AddorRemovePopup from '../components/AddorRemovePopup';
 import Calendar from '../components/Calendar';
 import CreateNewEvent from '../components/CreateNewEvent';
 
 import UsersListPopup from '../components/UsersListPopup';
 
-function User({ args }) {
+const User = function ({ args }) {
+  return (
+    <>
+      <h1> Welcome to Admin Page</h1>
+      <h4>
+        {' '}
+        Hello
+        {args.email}
+      </h4>
+      <UsersListPopup />
 
-    return (
-        <>
-            <h1> Welcome to Admin Page</h1>
-            <h4> Hello {args.email}</h4>
-            <UsersListPopup />
-
-
-
-            <AddorRemovePopup />
-            <CreateNewEvent />
-            <Calendar email={args.email} />
-
-        </>
-    );
-}
+      <AddorRemovePopup />
+      <CreateNewEvent />
+      <Calendar email={args.email} />
+    </>
+  );
+};
 
 export default User;
