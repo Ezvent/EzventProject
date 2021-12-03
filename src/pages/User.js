42c682dd0dca3adce11ee965ded9540afb0767ce
Shaheen <shaheensr@outlook.com>
2021-12-03 17:06:05 -0500

    final changes

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import Calendar from '../components/Calendar';
+import React from "react";
+import Calendar from "../components/Calendar";
 
 function User({ args }) {
-
   return (
     <>
       <h1> Welcome to User Page</h1>
       <h1> Hello {args.email}</h1>
       <Calendar email={args.email} />
     </>
-  )
+  );
 }
 
 export default User;
